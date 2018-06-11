c963a71c5f90bc488e062c1b5665e1cdef23d3e6
Saurav K Singh <sauravksingh@hotmail.com>
2018-06-11 16:59:52 +0530

    Add error logging

--- src/service/loggerservice.js
+++ src/service/loggerservice.js
@@ -1,6 +1,6 @@
 const fs = require("fs"),
     SumoLogger = require("sumo-logger"),
-    uuidv4 = require("uuid/v4");   
+    uuidv4 = require("uuid/v4");
 
 class LoggerService {
     constructor(opts) {
@@ -12,18 +12,30 @@ class LoggerService {
         return LoggerService.loggerInstance;
     }
 
-    initOptions(opts) {    
-        opts["sessionKey"] = uuidv4(); 
+    initOptions(opts) {
+        opts["sessionKey"] = uuidv4();
         opts["onSuccess"] = () => {};
-        opts["onError"] = (err) => {      
-            fs.appendFile("./loggingfailures.log",  err, 'utf8');
+        opts["onError"] = (err) => {
+            fs.appendFile("./loggingfailures.log", this.prepareErrorLog(err, opts), 'utf8', (err) => {
+                if (err) {
+                    throw new Error("Error in writing logging failures");
+                }
+            });
         };
         return opts;
     }
 
     log(message) {
         this.sumoLogger.log(message)
-    };    
+    };
+
+    prepareErrorLog(err, opts) {
+        let log = {};
+        log["timestamp"] = new Date().toISOString();
+        log["error"] = err;
+        log["opts"] = opts;
+        return log + "\r\n";
+    }
 }
 
 module.exports = LoggerService;
